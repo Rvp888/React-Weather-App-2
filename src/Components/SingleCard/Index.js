694ba3e371931af97d43711215ea9afa952dbcc4
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-10 18:26:33 +0530

    modify files

--- src/Components/SingleCard/Index.js
+++ src/Components/SingleCard/Index.js
@@ -1,13 +1,33 @@
 
 import React from "react";
+import dayjs from 'dayjs';
+
+
+
 
 const SingleCardComponents = ( {item, className, onClick} ) => {
+
+    const WEEKDAYS = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+
+    const weekdayindex = dayjs.unix(item.dt).day();
+        
 
     return (
         <>
             <li key={item.moonrise} className={className} onClick={onClick}>
-                
+                <img alt="rohit" className="day-icon" src={`http://openweathermap.org/img/wn/${item.weather[0]}@2x.png`} />
             </li>
         </>
     )
 }
+
+
+export default SingleCardComponents;
